91ca09804351da0262477b1e0079cdcea7bb944f
Mohit Chandel <devmohit93@gmail.com>
2022-03-24 13:32:23 +0530

    updated code

--- contracts/ERC721AMint.sol
+++ contracts/ERC721AMint.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "erc721a/contracts/ERC721A.sol";
 
-contract ERC721Mint is ERC721A {
+contract ERC721AMint is ERC721A {
     address admin;
     address reciever = 0x3799ff376455A3d095a21689B3D479DEBc1c49ED;
 
@@ -11,9 +11,9 @@ contract ERC721Mint is ERC721A {
         admin = msg.sender;
     }
 
-    function safeMint(uint256 tokenId) public {
-        _safeMint(admin, tokenId);
-        transferNFT(tokenId);
+    function safeMint(uint256 quantity) public {
+        _safeMint(admin, quantity);
+        transferNFT(0);
     }
 
     function transferNFT(uint256 _tokenId) internal {
