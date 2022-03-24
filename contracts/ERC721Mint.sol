ba3d6a40d6b7eababe0ff46fde3b76bbfd38ff10
Mohit Chandel <45650327+mohitchandel@users.noreply.github.com>
2022-03-24 10:16:29 +0530

    init

---
+++ contracts/ERC721Mint.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MyToken is ERC721 {
+
+    address admin;
+
+    constructor() ERC721("GameSword", "GMSRD") {
+        admin = msg.sender;
+        _mint(admin, 1);
+    }
+    
+    function transferNFT(uint256 _tokenId) external payable {
+        address seller = ownerOf(_tokenId);
+        _transfer(seller, msg.sender, _tokenId);
+    }
+}
